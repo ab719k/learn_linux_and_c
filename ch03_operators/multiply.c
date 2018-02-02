6d1ea8a58dd8bd7b70ba0b0da905c99fbabc77b6
Amit Bhatia <ab719k@att.com>
2018-02-02 13:13:09 -0500

    Adding operators

---
+++ ch03_operators/multiply.c
@@ -0,0 +1,42 @@
+/*
+ * @filename add.c
+ * @brief: Read two value and calculate their sum and display
+ *
+ * */
+
+
+#include <stdio.h>
+int main()
+{
+    // declaration and definition of float variables
+    // multiple declarations and definitions examples
+    //float speed, time, distance;
+    // 
+
+	// read a variable using scanf remeber we need to use 
+	// type specifier for reading and storing the variable
+	// we need to pass the address of the variable
+	// e.g. scanf("%c", &variable_name); is used to read a character
+	// 		c -> character
+	// 		d -> integer
+	// 		f -> float
+	// 		d -> double
+	// scanf();
+	
+	// Read the second number
+	// scanf();
+	
+	// Can you calculate the sum of numbers that you read
+	// we use + sign to add the two numbers
+	//
+ 
+    // Let us print a variable using printf
+    // you need to use the type specifier for printing as well
+    // e.g. printf("%c ", variable_name); to print a character 
+    // \n represent the newline
+    //printf("\n", );
+
+	
+ 
+    return 0;
+}

ba5ea79b5391c25f53ac1cb2d2cd8750fd6f8992
Amit Bhatia <ab719k@att.com>
2018-02-02 11:44:38 -0500

    Adding new directories and files

---
+++ ch02_io/int.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    // declaration and definition of variable 'a123'
+    // multiple declarations and definitions
+    int i, _d45, myValue; 
+
+ 
+    // Let us print a variable
+    printf("%c \n", a123);
+
+	
+ 
+    return 0;
+}

e9289df56a6b465aad57edde1584775bea0a17be
Amit Bhatia <amitbhatia76@gmail.com>
2018-02-06 18:26:05 -0500

    Update int.c

--- ch02_io/int.c
+++ ch02_io/int.c
@@ -9,22 +9,27 @@ int main()
 {
     // declaration and definition of integer variables
     // multiple declarations and definitions examples
-    //int i, _d45, myValue;
+    int i, _d45, myValue;
 
-	// read a variable using scanf remeber we need to use 
-	// type specifier for reading and storing the variable
-	// we need to pass the address of the variable
-	// e.g. scanf("%c", &variable_name); is used to read a character
-	// c -> character
-	// d -> integer
-	// f -> float
-	// d -> double
- 
+// read a variable using scanf remeber we need to use 
+// type specifier for reading and storing the variable
+// we need to pass the address of the variable
+// e.g. scanf("%c", &variable_name); is used to read a character
+// c -> character
+// d -> integer
+// f -> float
+// d -> double
+ 	scanf("%d", &i);
+	scanf();
+	scanf();
+	
     // Let us print a variable using printf
     // you need to use the type specifier for printing as well
     // e.g. printf("%c ", variable_name); to print a character 
     // \n represent the newline
-    //printf("\n", );
+    printf("%d\n", i);
+    printf();
+    printf();
 
 	
  
